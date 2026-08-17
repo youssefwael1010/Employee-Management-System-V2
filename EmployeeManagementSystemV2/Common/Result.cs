e6e719f43769a4aa5c6b8cd9d9d8a4afcd8a1468
youssef <207546198+youssefwael1010@users.noreply.github.com>
2026-08-17 16:20:40 +0300

    feat: add generic result type

---
+++ EmployeeManagementSystemV2/Common/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Common
+{
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = String.Empty;
+        public T? Data {  get; set; }
+
+    }
+}

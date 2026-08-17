c3c3550ed3915657deb29e736a1078f6fabcca8a
youssef <207546198+youssefwael1010@users.noreply.github.com>
2026-08-17 16:25:20 +0300

    feat: add company collections

---
+++ EmployeeManagementSystemV2/Services/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using  EmployeeManagementSystem.Models;
+using  EmployeeManagementSystem.Common;
+
+namespace EmployeeManagementSystem.Services
+{
+    public class Company
+    {
+        private readonly List<Employee> employees  = new();
+
+        private readonly Dictionary<int, Department> departments  = new();
+
+        private readonly Queue<Employee> onboardingQueue  = new();
+
+        private readonly Stack<string> actionHistory  = new();
+
+        private readonly HashSet<string> companySkills = new();
+
+    }
+
+    
+
+}

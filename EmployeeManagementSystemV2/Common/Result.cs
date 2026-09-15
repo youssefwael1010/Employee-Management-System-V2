1104dca061e4b0d91a7fe799a066f5fba26b58e1
youssef wael <207546198+youssefwael1010@users.noreply.github.com>
2026-09-16 01:14:38 +0300

    Refactor Result class for encapsulation and utility methods
    Updated properties to be private and added static methods for success and failure results.

--- EmployeeManagementSystemV2/Common/Result.cs
+++ EmployeeManagementSystemV2/Common/Result.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,23 @@ namespace EmployeeManagementSystem.Common
 {
     public class Result<T>
     {
-        public bool Success { get; set; }
-        public string Message { get; set; } = String.Empty;
-        public T? Data {  get; set; }
+
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+        public T? Data {  get; private set; }
+        private Result(bool success, string message, T? data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public static Result<T> SuccessResult(T data, string message="" ) =>
+         new Result<T>(true, message, data );
+
+        public static Result<T> Failure(string message) =>
+         new Result<T>(false, message, default);
+        
 
     }
 }

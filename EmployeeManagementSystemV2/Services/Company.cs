2b42f4cce884c42314f3e3bd207c972edccc2445
youssef wael <207546198+youssefwael1010@users.noreply.github.com>
2026-09-16 01:16:13 +0300

    Refactor Company class for improved readability

--- EmployeeManagementSystemV2/Services/Company.cs
+++ EmployeeManagementSystemV2/Services/Company.cs
@@ -1,14 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using  EmployeeManagementSystem.Delegates;
 using  EmployeeManagementSystem.Models;
+using  EmployeeManagementSystem.Events;
 using  EmployeeManagementSystem.Common;
-using EmployeeManagementSystem.Delegates;
-using EmployeeManagementSystem.Events;
 
 namespace EmployeeManagementSystem.Services
 {
@@ -23,18 +23,18 @@ namespace EmployeeManagementSystem.Services
         private readonly Stack<string> actionHistory  = new();
 
         private readonly HashSet<string> companySkills = new();
-
+        
         public event EventHandler<EmployeeEventArgs>? EmployeeOnBoarded;
 
-        public event EventHandler<EmployeeEventArgs>? EmployeePromoted
+        public event EventHandler<EmployeeEventArgs>? EmployeePromoted;
 
         // helper methods
         public Employee? FindEmployeeById(int id)
         {
-            foreach (Employee employee in employees)
+            foreach (Employee employee in employees) 
             {
-                if (employee.Id == id)
-                    return employee;
+                if(employee.Id == id)
+                return employee;
             }
             return null;
         }
@@ -58,119 +58,85 @@ namespace EmployeeManagementSystem.Services
 
             return null;
         }
+        private bool EmployeeIdExists(int id)
+                {
+                    foreach (Employee emp in employees)
+                    {
+                        if (emp.Id == id) return true;
+                    }
 
-        //
-        //
+                    foreach (Employee emp in onboardingQueue)
+                    {
+                        if (emp.Id == id) return true;
+                    }
 
+                    return false;
+                }
+       
+        
         public Result<Employee> AddEmployeeToOnboarding(Employee employee)
         {
             if (employee is null)
-                return new Result<Employee>
-                {
-                    Success = false,
-                    Message = "Employee not found.",
-                    Data = null
-                };
+                return Result<Employee>.Failure("Employee not found.");
 
             if (string.IsNullOrWhiteSpace(employee.Name))
-                return new Result<Employee>
-                {
-                    Success = false,
-                    Message = "Employee name is required.",
-                    Data = null
-                };
-            //throw new Exception("Employee name is required.");
+                return Result<Employee>.Failure("Employee name is required.");
 
-            if (employee.Salary < 0)
-                return new Result<Employee>
-                {
-                    Success = false,
-                    Message = "Salary cannot be negative.",
-                    Data = null
-                };
-            //throw new Exception("Salary cannot be negative.");
-
+            if (employee.Salary <= 0)
+                return Result<Employee>.Failure("Salary must be greater than zero.");
+   
             if (!departments.ContainsKey(employee.DepartmentId))
-                return new Result<Employee>
-                {
-                    Success = false,
-                    Message = $"Department Id {employee.DepartmentId} does not exist.",
-                    Data = null
-                };
-            //throw new InvalidOperationException($"Department Id {employee.DepartmentId} does not exist.");
+                return Result<Employee>.Failure($"Department Id {employee.DepartmentId} does not exist.");
+     
+            if (EmployeeIdExists(employee.Id))
+                return Result<Employee>.Failure($"Employee Id {employee.Id} already exists.");
 
-            if (FindEmployeeById(employee.Id) is not null)
-                return new Result<Employee>
-                {
-                    Success = false,
-                    Message = $"Employee Id {employee.Id} already exists.",
-                    Data = null
-                };
-            //throw new InvalidOperationException($"Employee Id {employee.Id} already exists.");
+            int maxId = 0;
+            foreach (var emp in employees)
+            {
+                if (emp.Id > maxId) maxId = emp.Id;
+            }
+            foreach (var emp in onboardingQueue)
+            {
+                if (emp.Id > maxId) maxId = emp.Id;
+            }
 
+            employee.Id = maxId + 1;
 
             onboardingQueue.Enqueue(employee);
             actionHistory.Push($"Employee added to onboarding: {employee.Name}");
 
-            return new Result<Employee>
-            {
-                Success = true,
-                Message = "Employee added Successfully",
-                Data = employee
-            };
+            return Result<Employee>.SuccessResult(employee, "Employee added Successfully");
+   
 
         }
         public Result<Department> AddDepartment(Department department)
         {
             if (department is null)
-                //throw new ArgumentNullException(nameof(department));
-                return new Result<Department>
-                {
-                    Success = false,
-                    Message = $"{nameof(department)} is required."
-                };
+               
+                return Result<Department>.Failure($"{nameof(department)} is required.");
 
             if (string.IsNullOrWhiteSpace(department.Name))
-                //throw new Exception("Department name is required.");
-                return new Result<Department>
-                {
-                    Success = false,
-                    Message = "Department name is required.",
-                    Data = null
-                };
+                
+                return Result<Department>.Failure("Department name is required.");
 
             if (departments.ContainsKey(department.Id))
-                //throw new InvalidOperationException($"Department Id {department.Id} already exists.");
-                return new Result<Department>
-                {
-                    Success = false,
-                    Message = $"Department Id {department.Id} already exists.",
-                    Data = null
-                };
+             
+                return  Result<Department>.Failure($"Department Id {department.Id} already exists.");
 
             departments.Add(department.Id, department);
 
             actionHistory.Push($"Added department: {department.Name}");
 
-            return new Result<Department>
-            {
-                Success = true,
-                Message = "Department added Successfully.",
-                Data = department
-            };
+            return Result<Department>.SuccessResult(department, "Department added Successfully.");
+          
 
         }
-
-        public Result<Employee> ProcessNextOnboarding()
+        public Result<Employee> ProcessNextOnboarding() 
         {
             if (onboardingQueue.Count == 0)
-                //throw new InvalidOperationException("No employees waiting for onboarding.");
-                return new Result<Employee>
-                {
-                    Success = false,
-                    Message = "No employees waiting for onboarding.",
-                    Data = null
-                };
+                return Result<Employee>.Failure("No employees waiting for onboarding.");
+             
             Employee employee = onboardingQueue.Dequeue();
 
 
@@ -179,17 +145,63 @@ namespace EmployeeManagementSystem.Services
 
             if (EmployeeOnBoarded is not null)
             {
-                EmployeeOnBoarded(this, new EmployeeEventArgs(employee));
+                EmployeeOnBoarded(this , new EmployeeEventArgs(employee));
+            }
+            
+
+            actionHistory.Push( $"Employee activated: {employee.Name}");
+            return Result<Employee>.SuccessResult(employee, "Employee OnBoarded Successfully.");
+            
+        }
+        public Result<Employee> AssignEmployeeToManager(int employeeId, int managerId)
+        {
+            Employee? employee = FindEmployeeById(employeeId);
+
+            if (employee is null)
+                return Result<Employee>.Failure(
+                    $"Employee with Id {employeeId} was not found.");
+
+
+            Employee? managerEmployee = FindEmployeeById(managerId);
+
+            if (managerEmployee is null)
+            {
+                return Result<Employee>.Failure(
+                    $"Manager with Id {managerId} was not found.");
+            }
+
+            if (managerEmployee is not Manager manager)
+            {
+                return Result<Employee>.Failure(
+                    "The selected employee is not a manager.");
             }
 
 
-            actionHistory.Push($"Employee activated: {employee.Name}");
-            return new Result<Employee>
+            if (employee.Id == manager.Id)
             {
-                Success = true,
-                Message = "Employee OnBoarded Successfully.",
-                Data = employee
-            };
+                return Result<Employee>.Failure(
+                    "A manager cannot be assigned to himself.");
+            }
+
+
+
+            foreach (Employee teamMember in manager.TeamMembers)
+            {
+                if (teamMember.Id== employee.Id)
+                {
+                    return Result<Employee>.Failure(
+                        "Employee is already assigned to this manager.");
+                }
+            }
+            manager.TeamMembers.Add(employee);
+
+            actionHistory.Push(
+                 $"Assigned {employee.Name} to manager {manager.Name}");
+
+            return Result<Employee>.SuccessResult(
+                employee,
+                $"Employee {employee.Name} assigned to manager {manager.Name}.");
+
         }
         public void AddSkillToEmployee(int employeeId, string skill)
         {
@@ -210,68 +222,12 @@ namespace EmployeeManagementSystem.Services
             actionHistory.Push($"Added skill {normalizedSkill} to {employee.Name}");
 
         }
-        public HashSet<string> GetCompanySkills()
+        public HashSet<string> GetCompanySkills() 
         {
             return companySkills;
         }
 
-        public List<Employee> FilterEmployees(EmployeeFilter filter)
-        {
-
-            List<Employee> result = new();
-            foreach (var employee in employees)
-            {
-                if (filter(employee))
-                {
-                    result.Add(employee);
-                }
-            }
-            return result;
-
-        }
-
-        public Result<Manager> PromoteToManager(int employeeId)
-        {
-
-            Employee? employee = FindEmployeeById(employeeId);
-
-            if (employee is null)
-                return new Result<Manager>
-                {
-                    Success = false,
-                    Message = "Employee not found.",
-                    Data = null
-                };
-
-            var manager = new Manager
-            {
-                Id = employeeId,
-                Name = employee.Name,
-                HireDate = employee.HireDate,
-                DepartmentId = employee.DepartmentId,
-                Salary = employee.Salary,
-                Skills = employee.Skills
-            };
-
-            int index = employees.IndexOf(employee);
-
-            employees[index] = manager;
-
-            actionHistory.Push($"Employee promoted: {manager.Name}");
-
-            if (EmployeePromoted is not null)
-                EmployeePromoted(this, new EmployeeEventArgs(manager));
-
-
-            return new Result<Manager>
-            {
-                Success = true,
-                Message = "Employee Promoted Successfully",
-                Data = manager
-            };
-        }
-
-        public Dictionary<string, int> GetDepartmentReport()
+        public Dictionary<string, int> GetDepartmentReport()  
         {
             if (departments.Count == 0)
                 throw new InvalidOperationException("No departments found.");
@@ -292,19 +248,21 @@ namespace EmployeeManagementSystem.Services
             }
             return report;
         }
-        public Stack<string> GetActionHistory()
+        public Stack<string> GetActionHistory() 
         {
             return actionHistory;
-        }
+        } 
         public decimal CalculateAverageSalary()
         {
+            if (employees.Count == 0)
+                return 0;
 
             decimal totalSalary = 0;
 
             foreach (var employee in employees)
-                totalSalary += employee.Salary;
+              totalSalary += employee.Salary;
 
-            return totalSalary / employees.Count;
+            return Math.Round(totalSalary / employees.Count,2);
         }
         public List<Employee> GetEmployeesByDepartment(int departmentId)
         {
@@ -313,18 +271,63 @@ namespace EmployeeManagementSystem.Services
             if (department is null)
                 throw new InvalidOperationException($"Department with Id {departmentId} was not found.");
 
-            List<Employee> empsDepartment = new();
+            List < Employee> empsDepartment = new ();
 
             foreach (var employee in employees)
             {
-                if (employee.DepartmentId == departmentId)
+                if(employee.DepartmentId == departmentId)
                     empsDepartment.Add(employee);
 
             }
 
             return empsDepartment;
         }
+        public Result<Manager> PromoteToManager(int employeeId)
+        {
 
+            Employee? employee = FindEmployeeById(employeeId);
+
+            if (employee is null)
+                return Result<Manager>.Failure("Employee not found.");
+              
+
+            var manager = new Manager
+            {
+                Id = employeeId,
+                Name = employee.Name,
+                HireDate = employee.HireDate,
+                DepartmentId = employee.DepartmentId,
+                Salary = employee.Salary,
+                Skills = new HashSet<string>(employee.Skills)
+            };
+
+            int index = employees.IndexOf(employee);
+
+            employees[index] = manager;
+
+            actionHistory.Push($"Employee promoted: {manager.Name}");
+
+            if (EmployeePromoted is not null)
+                EmployeePromoted(this, new EmployeeEventArgs(manager));
+
+
+            return Result<Manager>.SuccessResult(manager, "Employee Promoted Successfully");
+           
+        }
+        public List<Employee> FilterEmployees(EmployeeFilter filter)
+        {
+           
+            List<Employee> result = new();
+            foreach (var employee in employees)
+            {
+                if (filter(employee))
+                {
+                    result.Add(employee);
+                }
+            }
+            return result;
+
+        }
         public List<Employee> GetEmployees()
         {
             List<Employee> result = new();
@@ -335,29 +338,69 @@ namespace EmployeeManagementSystem.Services
             return result;
         }
 
-        public void SeedData()
+        public List<Employee> GetManagerTeam(int managerId)
         {
-            AddDepartment(new Department { Name = "IT", Id = 1 });
-            AddDepartment(new Department { Name = "HR", Id = 2 });
-            AddDepartment(new Department { Name = "Finance", Id = 3 });
+            Employee? employee = FindEmployeeById(managerId);
 
-            AddEmployeeToOnboarding(new Employee { Id = 1, Name = "Ahmed", HireDate = DateTime.Now, DepartmentId = 1, Salary = 15000 });
-            AddEmployeeToOnboarding(new Employee { Id = 2, Name = "Ali", HireDate = DateTime.Now, DepartmentId = 2, Salary = 7000 });
-            AddEmployeeToOnboarding(new Employee { Id = 3, Name = "Sara", HireDate = DateTime.Now, DepartmentId = 1, Salary = 15000 });
+            if (employee is null)
+                return new List<Employee>();
 
-            //ProcessNextOnboarding();
-            //ProcessNextOnboarding();
-            //ProcessNextOnboarding();
+            if (employee is not Manager manager)
+                return new List<Employee>();
 
-            //AddSkillToEmployee(1, "C#");
-            //AddSkillToEmployee(1, ".NET");
-            //AddSkillToEmployee(2, "Excel");
-            //AddSkillToEmployee(3, "SQL");
+            return manager.TeamMembers;
         }
 
 
+        public void SeedData()
+        {
+
+            AddDepartment(new Department { Id = 1, Name = "IT" });
+            AddDepartment(new Department { Id = 2, Name = "HR" });
+            AddDepartment(new Department { Id = 3, Name = "Finance" });
+            AddDepartment(new Department { Id = 4, Name = "Marketing" });
+
+
+
+            AddEmployeeToOnboarding(new Employee { Name = "Ahmed", HireDate = DateTime.Now.AddMonths(-6), DepartmentId = 1, Salary = 12000 });
+            AddEmployeeToOnboarding(new Employee { Name = "Ali", HireDate = DateTime.Now.AddMonths(-4), DepartmentId = 2, Salary = 8000 });
+            AddEmployeeToOnboarding(new Employee { Name = "Sara", HireDate = DateTime.Now.AddMonths(-5), DepartmentId = 1, Salary = 15000 });
+            AddEmployeeToOnboarding(new Employee { Name = "Omar", HireDate = DateTime.Now.AddMonths(-2), DepartmentId = 3, Salary = 9500 });
+            AddEmployeeToOnboarding(new Employee { Name = "Mona", HireDate = DateTime.Now.AddMonths(-1), DepartmentId = 4, Salary = 11000 });
+
+
+
+
+            //AddEmployeeToOnboarding(new Employee {Id = 1,Name = "Ahmed",HireDate = DateTime.Now,DepartmentId = 1,Salary = 15000});
+            //AddEmployeeToOnboarding(new Employee {Id = 2,Name = "Ali"  ,HireDate = DateTime.Now,DepartmentId = 2,Salary = 7000 });
+            //AddEmployeeToOnboarding(new Employee {Id = 3,Name = "Sara" ,HireDate = DateTime.Now,DepartmentId = 1,Salary = 15000});
+
+            ProcessNextOnboarding();
+            ProcessNextOnboarding();
+            ProcessNextOnboarding();
+
+
+            AddSkillToEmployee(1, "C#");
+            AddSkillToEmployee(1, ".NET");
+            AddSkillToEmployee(1, "SQL");
+
+            AddSkillToEmployee(2, "Communication");
+            AddSkillToEmployee(2, "Excel");
+
+            AddSkillToEmployee(3, "Angular");
+            AddSkillToEmployee(3, "C#");
+
+
+            PromoteToManager(1);
+
+            AssignEmployeeToManager(2, 1); 
+            AssignEmployeeToManager(3, 1);
+
+
+        }
+
     }
 
-
+    
 
 }
